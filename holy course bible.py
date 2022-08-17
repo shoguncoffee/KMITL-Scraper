1431d8a290cab112dbe60576bca4dcb6dce466ac
shoguncoffee <63220735+shoguncoffee@users.noreply.github.com>
2022-08-17 11:15:44 +0700

    new update

--- holy course bible.py
+++ holy course bible.py
@@ -18,8 +18,14 @@ def log(*s, t=None, multiline=False, end='\n', sep=' '):
         
     print('%-7s'%t, a, sep.join(str(q) for q in s), end=end, sep='')
 
-class TooManyRequests(Exception): pass
-class RedoThisLoop(Exception): pass
+class FormatError(Exception): 
+    def __init__(self, arg: object) -> None:
+        self.arg = arg
+        super().__init__(arg)
+    
+    def __str__(self) -> str:
+        return self.arg
+
 
 class csv_handle: # for overwrite method (unusable yet)
     field = [
@@ -92,76 +98,84 @@ class csv_handle: # for overwrite method (unusable yet)
         return k
 
 
-class subjects_detection:    
+class Subjects_detection:
+    host = 'k8s.reg.kmitl.ac.th'
+    URL = f'https://{host}/'
+    directory_token = 'api/user/'
+    directory_data = 'reg/api/' 
+    common_header = {
+        'Accept-Language': 'en-us',
+        'Host': host,
+        'Referer': 'https://new.reg.kmitl.ac.th/',
+        'Origin': 'https://new.reg.kmitl.ac.th',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive'
+        }
+    
     lookup_id = [
             # ('01-89', '01-99', '1-9'),
             # ('91-99', '01-99', '1-9'),
             ('01', '07', '6'),
             ('90', ('64','59','57'), '1-5'),
             ]
-        # ------ example ------ (No. run from 001 to 999)
+        # -------- example -------- (No. run from 001 to 999)
         # 01 07 6 xxx -> CE subject
         # maingroup branch degree No.
         # 
         # 90 64 5 xxx -> gen-ed subject
         # maingroup year group No.
     
-    def __init__(
-            self, 
-            student_id, 
-            password,
-            year = 2565,
-            semester = 1,
-            lookup_id = lookup_id,
-            level = 1,
-            csv = csv_handle # False = Not export to csv
-            ):
-        
-        errorlist = [
+    reason_why_cannot = [
             ('not in the course schedule', 'Close'),
             ('not pass rule in the course', 'Not qualified'),
             ('not still registered subject (Prerequisite)', 'Require prerequisite'),
             ('Not found Data', 'N/A'),
             ]
 
-        exception = (
+    exception = (
             requests.exceptions.ConnectTimeout,
             requests.exceptions.ConnectionError,
             urllib3.exceptions.ProtocolError,
             http.client.RemoteDisconnected,
             #aiohttp.client_exceptions.ClientConnectorError,
             )
-
-        common_header = {
-            'Accept-Language': 'en-us',
-            'Host': 'k8s.reg.kmitl.ac.th',
-            'Referer': 'https://new.reg.kmitl.ac.th/',
-            'Origin': 'https://new.reg.kmitl.ac.th',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Connection': 'keep-alive'
+    
+    
+    def __init__(self, student_id, password,
+            year = 2565, semester = 1, level = 1,
+            lookup_id = lookup_id, csv = csv_handle): # if csv = False -> Not export to csv
+        
+        registered_url_params = {
+            'funtion': 'get-regis-result',
+            'student_id': student_id,
+            'year': year,
+            'semester': semester,
+            'level_id': level,
             }
-
-        registered_url = 'https://k8s.reg.kmitl.ac.th/reg/api/?function=get-regis-result&' \
-            f'student_id={student_id}&year={year}&semester={semester}&level_id={level}'
-        
-        token_url = 'https://k8s.reg.kmitl.ac.th/api/user/'
-        
+        core_url_params = lambda id:{
+            'funtion': 'get-can-register-by-student-id-and-subject-id',
+            'subject_id': id,
+            'student_id': student_id,
+            'year': year,
+            'semester': semester,
+            }
         token_json = json.dumps({
             'function': 'login-jwt',
             'email': f'{student_id}@kmitl.ac.th',
             'password': password
-            })
-        
-        token_header = common_header | {
-                    'Content-Length': str(token_json.__len__()),
-                    'Accept': 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json;charset=utf-8'
-                    }
-        
+            })        
+        token_header = self.common_header | {
+            'Content-Length': str(token_json.__len__()),
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json;charset=utf-8'
+            }
         for k, v in locals().items():
             if k != 'self': setattr(self, k, v)
     
     
+    def get_url(self, directory):
+        return self.URL + directory
+        
     @staticmethod
     def time_period(t):
         tl = '12:00', '16:00', '19:00'
@@ -182,12 +196,6 @@ class subjects_detection:
         else: yield from s
     
     
-    def minion_url(self, id):
-        return \
-            'https://k8s.reg.kmitl.ac.th/reg/api/?function=get-can-register-by-student-id-and-subject-id'\
-            f'&subject_id={id}&student_id={self.student_id}&year={self.year}&semester={self.semester}'
-    
-    
     def id_selecter(self, id):        
         condition = (
             True
@@ -196,16 +204,15 @@ class subjects_detection:
             return True
     
     
-    def generate_id(self):            
-        for r in self.lookup_id:
-            l = (*r, '001-999') if len(r) == 3 else r
-            seq = [self.str_range(q) for q in l]
-            for d in product(*seq):
-                id = ''.join(d)
-                if self.id_selecter(id): yield id
+    def generate_id(self, r):            
+        l = (*r, '001-999') if len(r) == 3 else r
+        seq = [self.str_range(q) for q in l]
+        for d in product(*seq):
+            id = ''.join(d)
+            if self.id_selecter(id): yield id
 
 
-    def occupy_time(self, registered):
+    def get_occupy(self, registered):
         k = {'teach':{}, 'mexam':{}, 'exam':{}}
         time_period = self.time_period
         
@@ -231,7 +238,7 @@ class subjects_detection:
     
     def check_overlap(self, datalist, seq):
         time_period = self.time_period
-        occ = self.occ
+        occ = self.occupied_time
         
         def sub(subject):
             for s in 'mexam', 'exam':
@@ -290,7 +297,7 @@ class subjects_detection:
     def handle_respone(self, id, error, data):
         if error is not None:
             Err = error['message_en']                                     
-            for part, reply in self.errorlist:
+            for part, reply in self.reason_why_cannot:
                 if part in Err: 
                     log(f'{id}: {reply}')
                     break
@@ -300,66 +307,72 @@ class subjects_detection:
             self.apply_respone(id, data)
     
     
-    async def minion(self, id, session): 
-        async with session.get(self.minion_url(id)) as res:
+    async def core(self, id): 
+        async with self.session.get(self.core_url(id)) as res:
             rawdata = json.loads(await res.text())
         
         try: 
             error, infomation = rawdata['error'], rawdata['data']
         except KeyError:
             if 'many request' in rawdata:
-                raise TooManyRequests
+                raise FormatError('TooManyRequest')
             else:
                 log(f'{id}: Respone format Error', json.dumps(rawdata, indent=3), sep='\n', end='\n\n')
-                raise RedoThisLoop
+                raise FormatError('')
         else:
             self.handle_respone(id, error, infomation)
 
 
-    async def loop(self, session, seq_subject_id):
+    async def try_loop(self, id):
         while 1:
             try: 
-                await self.main(seq_subject_id, session)
-            except RedoThisLoop: 
-                continue            
-            except TooManyRequests as e:
-                log(e, ', reconnect in 12 sec: ')
-                for _ in range(12):
-                    sleep(1)
-                    print('.', end='')
-                print()
+                await self.core(id)
+            except FormatError as e:
+                if e == 'TooManyRequest':
+                    log(e, ', reconnect in 12 sec: ')
+                    for _ in range(12):
+                        sleep(1)
+                        print('.', end='')
+                    print()
+                
             except self.exception as e: 
-                log(f'{seq_subject_id}: Connection Error', e, sep='\n', end='\n\n')
+                log(f'{id}: Connection Error', e, sep='\n', end='\n\n')
             else: 
                 break
     
     
     async def main(self, token):
-        h = self.common_header | {
-            'Authorization': f"Bearer {token}",
-            'Accept': '*/*',
-            }
-        c = aiohttp.TCPConnector(limit=16)
-        t = aiohttp.ClientTimeout(total=5)
-        
-        async with aiohttp.ClientSession(header=h, connector=c, timeout=t) as session:
-            my_registered =  await session.get(self.registered_url)
-            self.registered_data = json.loads(await my_registered.text())
-            self.occ = self.occupy_time(self.registered_data) 
+        kwarg = {
+            'header': self.common_header | {
+                'Authorization': f"Bearer {token}",
+                'Accept': '*/*'
+                },
+            'connector': aiohttp.TCPConnector(limit=16),
+            'timeout': aiohttp.ClientTimeout(total=8),
+        }
+        async with aiohttp.ClientSession(**kwarg) as session:
+            my_registered =  await session.get(self.get_url(self.directory_data), 
+                                               params=self.registered_url_params
+                                               )
+            self.registered_class = json.loads(await my_registered.text())
+            self.occupied_time = self.get_occupy(self.registered_class) 
+            self.session = session
             
-            await asyncio.gather(
-                *[self.loop(session, seq_subject_id) for seq_subject_id in self.generate_id()]
-                )
+            for r in self.lookup_id:
+                await asyncio.gather(
+                    *[self.try_loop(id) for id in self.generate_id(r)]
+                    )
     
 
     def run(self):
         try:
             file = False
-            res = requests.post(self.token_url, self.token_json, headers=self.token_header)
+            res = requests.post(self.get_url(self.directory_token), 
+                data=self.token_json, headers=self.token_header
+                )
             token = json.loads(res.text)['token']
         except KeyError: 
             log('student-id or password incorrect')
-            # os._exit(1)
         else:
             if self.csv: 
                 file = open('data_table.csv', 'w')
@@ -368,11 +381,11 @@ class subjects_detection:
             asyncio.run(self.main(token))
         finally:
             res.close()
-            if self.csv and file: file.close()
+            if file: file.close()
 
 
 if __name__ == '__main__':
     id = ''
     password = ''
-    mysubject = subjects_detection(id, password, csv=False)
+    mysubject = Subjects_detection(id, password, csv=False)
     mysubject.run()
